544125faec2cdef43d6af96b15a7dfb9dc5fea97
Sachin <sachin333777@outlook.com>
2023-12-05 19:33:16 +0530

    update packages

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ function App() {
 
   return (
     <>
-      <h1 className=" bg-red-300 text-black">hello world</h1>
+      <h1 className=" bg-red-300 text-xl text-black font-main ">hello world</h1>
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,25 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dark-cyan': 'hsl(180, 29%, 50%)',
+        'light-grayish-bg': 'hsl(180, 31%, 95%)',
+        'grayish-cyan': 'hsl(180, 8%, 52%)',
+        'dark-grayish-cyan': 'hsl(180, 14%, 20%)'
+
+      },
+      fontSize: {
+        'fs-15': '0.9375rem'
+      },
+      fontWeight: {
+        'fw-500': '500',
+        'fw-700': '700'
+      },
+      fontFamily: {
+        main: ['League Spartan']
+      }
+    },
   },
   plugins: [],
 }
